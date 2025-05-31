7ab14b0994709ba7a56d94b90985f76c2111f1e4
Thanasis Prifti <thanasis.prifti@gmail.com>
2025-05-31 15:16:47 +0300

    Added Bookings functionality

--- app/src/main/java/com/example/theaterchatappnew2/BookingDetailsActivity.java
+++ app/src/main/java/com/example/theaterchatappnew2/BookingDetailsActivity.java
@@ -9,17 +9,17 @@ public class BookingDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_booking_details);
+        //setContentView(R.layout.activity_booking_details);
 
-        TextView title = findViewById(R.id.detailTitle);
-        TextView code = findViewById(R.id.detailCode);
-        TextView date = findViewById(R.id.detailDate);
-        TextView tickets = findViewById(R.id.detailTickets);
+        //TextView title = findViewById(R.id.detailTitle);
+        //TextView code = findViewById(R.id.detailCode);
+        //TextView date = findViewById(R.id.detailDate);
+        //TextView tickets = findViewById(R.id.detailTickets);
 
         Intent intent = getIntent();
-        title.setText(intent.getStringExtra("booking_title"));
-        code.setText(intent.getStringExtra("booking_code"));
-        date.setText(intent.getStringExtra("booking_date"));
-        tickets.setText(intent.getIntExtra("booking_tickets", 0) + " Tickets");
+        //title.setText(intent.getStringExtra("booking_title"));
+        //code.setText(intent.getStringExtra("booking_code"));
+        //date.setText(intent.getStringExtra("booking_date"));
+        //tickets.setText(intent.getIntExtra("booking_tickets", 0) + " Tickets");
     }
 }
